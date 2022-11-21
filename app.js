f8fba57a61985fcdb7a6fc40a86278be3228e307
AmelZannouh <118676957+AmelZannouh@users.noreply.github.com>
2022-11-21 21:12:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
-let openBtn = document.querySelector('#open');
+
 let closeBtn = document.querySelector('#close');
+let openBtn = document.querySelector('#open');
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
 
@@ -11,4 +12,5 @@ openBtn.addEventListener("click",() => {
 
 closeBtn.addEventListener("click",() =>{
     modal.style.display ="none"; 
+    overlay.style.opacity="0%";
 })
