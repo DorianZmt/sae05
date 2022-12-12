ff40a51f65932bc5389370d1c2a725e1c5f44a70
DorianZmt <99679606+DorianZmt@users.noreply.github.com>
2022-12-12 15:37:20 +0100

    Add files via upload

---
+++ sae05-main-4/js/accueil.js
@@ -0,0 +1,8 @@
+function toggle_text(id) {
+  var span = document.getElementById(id);
+  if(span.style.display == "none") {
+    span.style.display = "inline";
+  } else {
+    span.style.display = "none";
+  }
+}
